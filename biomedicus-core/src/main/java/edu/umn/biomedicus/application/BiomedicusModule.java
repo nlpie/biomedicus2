8eacffaf0c91ec3a251339907036d79554dc46a4
Ben Knoll <knol0061@umn.edu>
2016-04-12 17:49:44 -0500

    Document scoping

--- biomedicus-core/src/main/java/edu/umn/biomedicus/application/BiomedicusModule.java
+++ biomedicus-core/src/main/java/edu/umn/biomedicus/application/BiomedicusModule.java
@@ -3,6 +3,7 @@ package edu.umn.biomedicus.application;
 import com.google.inject.AbstractModule;
 import edu.umn.biomedicus.annotations.DocumentScoped;
 import edu.umn.biomedicus.annotations.ProcessorScoped;
+import edu.umn.biomedicus.common.text.Document;
 
 /**
  *
@@ -12,5 +13,7 @@ class BiomedicusModule extends AbstractModule {
     protected void configure() {
         bindScope(DocumentScoped.class, BiomedicusScopes.DOCUMENT_SCOPE);
         bindScope(ProcessorScoped.class, BiomedicusScopes.PROCESSOR_SCOPE);
+
+        bind(Document.class).toProvider(BiomedicusScopes.providedViaSeeding()).in(BiomedicusScopes.DOCUMENT_SCOPE);
     }
 }
