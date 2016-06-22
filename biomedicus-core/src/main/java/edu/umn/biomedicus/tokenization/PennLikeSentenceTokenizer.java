733cc82b805a2b280cc3f4060b51f349f9540165
Ben Knoll <knol0061@umn.edu>
2016-06-22 09:20:57 -0500

    Refactored Penn-like tokenizer to make it easier to test. Added a test.

---
+++ biomedicus-core/src/main/java/edu/umn/biomedicus/tokenization/PennLikeSentenceTokenizer.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2016 Regents of the University of Minnesota.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.umn.biomedicus.tokenization;
+
+import edu.umn.biomedicus.common.text.Span;
+import edu.umn.biomedicus.common.text.SpanLike;
+
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+class PennLikeSentenceTokenizer {
+    /**
+     * Any sequence of 1 or more character that are not unicode whitespace.
+     */
+    private static final Pattern WORDS = Pattern.compile("([^\\p{Z}\\n\\t\\r]+)");
+
+
+    private static final Pattern TRAILING_PERIOD = Pattern.compile("(\\.)$");
+
+    /**
+     * Break words apart whenever the unicode Dash Punctuation group (Pd) appears in them.
+     */
+    private static final Pattern MID_BREAKS = Pattern.compile("([\\p{Pd}/\\\\])");
+
+    /**
+     * Break the unicode Ps (open brackets) and Pi (open quotation).
+     * Break the unicode currency symbols Sc.
+     */
+    private static final Pattern BEGIN_BREAKS = Pattern.compile("^((\\p{Ps})|(\\p{Pi})|" +
+            "(\\p{Sc}))");
+
+    /**
+     * Break possessives and contractions ', 's, n't, 'll, 've, 're in both uppercase and lowercase forms.
+     * Break the unicode Pe (close brackets) and Pf (final quotation).
+     * Break all unicode punctuation (P) except period.
+     * Break the unicode currency symbols Sc.
+     */
+    private static final Pattern END_BREAKS = Pattern.compile(
+            "((')|('[SsDdMm])|(n't)|(N'T)|('ll)|('LL)|('ve)|('VE)|('re)|('RE)|" +
+                    "(\\p{Pe})|(\\p{Pf})|" +
+                    "([\\p{P}&&[^\\.]])|" +
+                    "(\\p{Sc}))$"
+    );
+
+    private final String sentenceText;
+
+    PennLikeSentenceTokenizer(String sentenceText) {
+        this.sentenceText = sentenceText;
+    }
+
+    Stream<TokenCandidate> startStreamWithWords() {
+        Stream.Builder<TokenCandidate> builder = Stream.builder();
+
+        Matcher characters = Pattern.compile("[^\\p{Z}\\n\\t\\r]").matcher(sentenceText);
+        int last = -1;
+        while (characters.find()) {
+            last = characters.end();
+        }
+
+        if (last == -1) {
+            return Stream.empty();
+        }
+
+        Matcher words = WORDS.matcher(sentenceText);
+        while (words.find()) {
+            int start = words.start();
+            int end = words.end();
+            boolean isLast = end == last;
+            TokenCandidate tokenCandidate = new TokenCandidate(start, end, isLast);
+            builder.add(tokenCandidate);
+        }
+        return builder.build();
+    }
+
+    Stream<TokenCandidate> splitTrailingPeriod(TokenCandidate tokenCandidate) {
+        if (!tokenCandidate.isLast) {
+            return Stream.of(tokenCandidate);
+        } else {
+            Matcher matcher = TRAILING_PERIOD.matcher(tokenCandidate.getCovered(sentenceText));
+            if (matcher.find()) {
+                return Stream.of(new TokenCandidate(tokenCandidate.derelativize(new Span(0, matcher.start())), false),
+                        new TokenCandidate(tokenCandidate.derelativize(new Span(matcher.start(), matcher.end())), true));
+            } else {
+                return Stream.of(tokenCandidate);
+            }
+        }
+    }
+
+    Stream<TokenCandidate> splitWordByMiddleBreaks(TokenCandidate tokenCandidate) {
+        CharSequence tokenText = tokenCandidate.getCovered(sentenceText);
+
+        Stream.Builder<TokenCandidate> builder = Stream.builder();
+
+        Matcher midBreaksMatcher = MID_BREAKS.matcher(tokenText);
+        if (midBreaksMatcher.find()) {
+            int begin = midBreaksMatcher.start();
+            int end = midBreaksMatcher.end();
+
+            Span beginSplit = tokenCandidate.derelativize(new Span(0, begin));
+            builder.add(new TokenCandidate(beginSplit, false));
+
+            Span matchedSplit = tokenCandidate.derelativize(new Span(begin, end));
+            builder.add(new TokenCandidate(matchedSplit, false));
+
+            while (midBreaksMatcher.find()) {
+                begin = midBreaksMatcher.start();
+                Span beforeSplit = tokenCandidate.derelativize(new Span(end, begin));
+                builder.add(new TokenCandidate(beforeSplit, false));
+
+                end = midBreaksMatcher.end();
+                matchedSplit = tokenCandidate.derelativize(new Span(begin, end));
+                builder.add(new TokenCandidate(matchedSplit, false));
+            }
+            Span lastSplit = tokenCandidate.derelativize(new Span(end, tokenText.length()));
+            builder.add(new TokenCandidate(lastSplit, tokenCandidate.isLast));
+        } else {
+            builder.add(tokenCandidate);
+        }
+        return builder.build();
+    }
+
+    Stream<TokenCandidate> splitWordByBeginBreaks(TokenCandidate tokenCandidate) {
+        Stream.Builder<TokenCandidate> builder = Stream.builder();
+        while (true) {
+            CharSequence tokenText = tokenCandidate.getCovered(sentenceText);
+            Matcher beginBreaksMatcher = BEGIN_BREAKS.matcher(tokenText);
+            if (beginBreaksMatcher.find()) {
+                assert beginBreaksMatcher.start() == 0;
+                Span begin = tokenCandidate.derelativize(new Span(beginBreaksMatcher.start(), beginBreaksMatcher.end()));
+                builder.add(new TokenCandidate(begin, false));
+
+                Span rest = tokenCandidate.derelativize(new Span(beginBreaksMatcher.end(), tokenText.length()));
+                tokenCandidate = new TokenCandidate(rest, tokenCandidate.isLast);
+            } else {
+                builder.add(tokenCandidate);
+                return builder.build();
+            }
+        }
+    }
+
+    Stream<TokenCandidate> splitWordByEndBreaks(TokenCandidate tokenCandidate) {
+        LinkedList<TokenCandidate> candidates = new LinkedList<>();
+
+        while (true) {
+            CharSequence tokenText = tokenCandidate.getCovered(sentenceText);
+            Matcher endBreaksMatcher = END_BREAKS.matcher(tokenText);
+            if (endBreaksMatcher.find()) {
+                Span rest = tokenCandidate.derelativize(new Span(0, endBreaksMatcher.start()));
+                tokenCandidate = new TokenCandidate(rest, false);
+                Span endSplit = tokenCandidate.derelativize(new Span(endBreaksMatcher.start(), endBreaksMatcher.end()));
+                candidates.addFirst(new TokenCandidate(endSplit, tokenCandidate.isLast));
+            } else {
+                candidates.addFirst(tokenCandidate);
+                return candidates.stream();
+            }
+        }
+    }
+
+    class TokenCandidate implements SpanLike {
+        private final int begin;
+        private final int end;
+        private final boolean isLast;
+
+        TokenCandidate(Span span, boolean isLast) {
+            begin = span.getBegin();
+            end = span.getEnd();
+            this.isLast = isLast;
+        }
+
+        TokenCandidate(int begin, int end, boolean isLast) {
+            this.begin = begin;
+            this.end = end;
+            this.isLast = isLast;
+        }
+
+        @Override
+        public int getBegin() {
+            return begin;
+        }
+
+        @Override
+        public int getEnd() {
+            return end;
+        }
+
+        public boolean isLast() {
+            return isLast;
+        }
+    }
+}
--- biomedicus-core/src/main/java/edu/umn/biomedicus/tokenization/PennLikeTokenizer.java
+++ biomedicus-core/src/main/java/edu/umn/biomedicus/tokenization/PennLikeTokenizer.java
@@ -26,45 +26,11 @@ import edu.umn.biomedicus.common.text.Sentence;
 import edu.umn.biomedicus.common.text.Span;
 import edu.umn.biomedicus.exc.BiomedicusException;
 
-import java.util.LinkedList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Iterator;
 
 @DocumentScoped
 public class PennLikeTokenizer implements DocumentProcessor {
     private final Document document;
-
-    /**
-     * Any sequence of 1 or more character that are not unicode whitespace.
-     */
-    private static final Pattern WORDS = Pattern.compile("([^\\p{Z}\\n\\t\\r]+)");
-
-    /**
-     * Break the unicode Ps (open brackets) and Pi (open quotation).
-     * Break the unicode currency symbols Sc.
-     */
-    private static final Pattern BEGIN_BREAKS = Pattern.compile("^((\\p{Ps})|(\\p{Pi})|" +
-            "(\\p{Sc}))");
-
-    /**
-     * Break words apart whenever the unicode Dash Punctuation group (Pd) appears in them.
-     */
-    private static final Pattern MID_BREAKS = Pattern.compile("([\\p{Pd}/\\\\])");
-
-    /**
-     * Break possessives and contractions ', 's, n't, 'll, 've, 're in both uppercase and lowercase forms.
-     * Break the unicode Pe (close brackets) and Pf (final quotation).
-     * Break all unicode punctuation (P) except period.
-     * Break the unicode currency symbols Sc.
-     */
-    private static final Pattern END_BREAKS = Pattern.compile(
-            "((')|('[SsDdMm])|(n't)|(N'T)|('ll)|('LL)|('ve)|('VE)|('re)|('RE)|" +
-                    "(\\p{Pe})|(\\p{Pf})|" +
-                    "([\\p{P}&&[^\\.]])|" +
-                    "(\\p{Sc}))$"
-    );
-
-    private static final Pattern TRAILING_PERIOD = Pattern.compile("(\\.)$");
     private final Labeler<ParseToken> parseTokenLabeler;
 
     @Inject
@@ -79,112 +45,19 @@ public class PennLikeTokenizer implements DocumentProcessor {
         for (Sentence sentence : document.getSentences()) {
             String text = sentence.getText();
 
-            // phase 1, split by whitespace
+            PennLikeSentenceTokenizer sentenceTokenizer = new PennLikeSentenceTokenizer(text);
 
-            LinkedList<Span> splitByWhitespace = new LinkedList<>();
-
-            Matcher words = WORDS.matcher(text);
-            while (words.find()) {
-                splitByWhitespace.addLast(new Span(words.start(), words.end()));
-            }
-
-            if (splitByWhitespace.size() == 0) {
-                return;
-            }
-
-            // phase 1.1 split a trailing period
-
-            Span lastTokenByWhiteSpace = splitByWhitespace.pollLast();
-            Matcher matcher = TRAILING_PERIOD.matcher(lastTokenByWhiteSpace.getCovered(text));
-            if (matcher.find()) {
-                splitByWhitespace.addLast(lastTokenByWhiteSpace.derelativize(new Span(0, matcher.start())));
-                splitByWhitespace.add(lastTokenByWhiteSpace.derelativize(new Span(matcher.start(), matcher.end())));
-            } else {
-                splitByWhitespace.addLast(lastTokenByWhiteSpace);
-            }
-
-            // phase 2 split by middle breaking characters
-
-            LinkedList<Span> splitByWhitespaceAndMid = new LinkedList<>();
-
-            while (!splitByWhitespace.isEmpty()) {
-                Span poll = splitByWhitespace.pollFirst();
-
-                CharSequence tokenText = poll.getCovered(text);
-
-                Matcher midBreaksMatcher = MID_BREAKS.matcher(tokenText);
-                if (midBreaksMatcher.find()) {
-                    int begin = midBreaksMatcher.start();
-                    int end = midBreaksMatcher.end();
-
-                    Span beginSplit = poll.derelativize(new Span(0, begin));
-                    splitByWhitespaceAndMid.addLast(beginSplit);
-
-                    Span matchedSplit = poll.derelativize(new Span(begin, end));
-                    splitByWhitespaceAndMid.addLast(matchedSplit);
-
-                    while (midBreaksMatcher.find()) {
-                        begin = midBreaksMatcher.start();
-                        Span beforeSplit = poll.derelativize(new Span(end, begin));
-                        splitByWhitespaceAndMid.addLast(beforeSplit);
-
-                        end = midBreaksMatcher.end();
-                        matchedSplit = poll.derelativize(new Span(begin, end));
-                        splitByWhitespaceAndMid.addLast(matchedSplit);
-                    }
-                    Span lastSplit = poll.derelativize(new Span(end, tokenText.length()));
-                    splitByWhitespaceAndMid.addLast(lastSplit);
-                } else {
-                    splitByWhitespaceAndMid.add(poll);
-                }
-            }
-
-            // phase 3 split off begin-breaking sequences
-
-            LinkedList<Span> splitByWhitespaceBeginAndMid = new LinkedList<>();
-
-            while (!splitByWhitespaceAndMid.isEmpty()) {
-                Span poll = splitByWhitespaceAndMid.pollFirst();
-
-                CharSequence tokenText = poll.getCovered(text);
-
-                Matcher beginBreaksMatcher = BEGIN_BREAKS.matcher(tokenText);
-                if (beginBreaksMatcher.find()) {
-                    assert beginBreaksMatcher.start() == 0;
-                    Span begin = poll.derelativize(new Span(beginBreaksMatcher.start(), beginBreaksMatcher.end()));
-                    splitByWhitespaceBeginAndMid.addLast(begin);
-
-                    Span rest = poll.derelativize(new Span(beginBreaksMatcher.end(), tokenText.length()));
-                    splitByWhitespaceAndMid.addFirst(rest);
-                } else {
-                    splitByWhitespaceBeginAndMid.addLast(poll);
-                }
-            }
-
-            // phase 4 split off end-breaking sequences
-
-            LinkedList<Span> allSplit = new LinkedList<>();
-
-            while (!splitByWhitespaceBeginAndMid.isEmpty()) {
-                Span poll = splitByWhitespaceBeginAndMid.pollLast();
-                CharSequence tokenText = poll.getCovered(text);
-
-                Matcher endBreaksMatcher = END_BREAKS.matcher(tokenText);
-                if (endBreaksMatcher.find()) {
-                    Span rest = poll.derelativize(new Span(0, endBreaksMatcher.start()));
-                    splitByWhitespaceBeginAndMid.addLast(rest);
-                    Span endSplit = poll.derelativize(new Span(endBreaksMatcher.start(), endBreaksMatcher.end()));
-                    allSplit.addFirst(endSplit);
-                } else {
-                    allSplit.addFirst(poll);
-                }
-            }
-
-            // phase 5 label the splits created through the previous phases
+            Iterator<PennLikeSentenceTokenizer.TokenCandidate> iterator = sentenceTokenizer.startStreamWithWords()
+                    .flatMap(sentenceTokenizer::splitTrailingPeriod)
+                    .flatMap(sentenceTokenizer::splitWordByMiddleBreaks)
+                    .flatMap(sentenceTokenizer::splitWordByBeginBreaks)
+                    .flatMap(sentenceTokenizer::splitWordByEndBreaks)
+                    .iterator();
 
             Span last = null;
-            while (!allSplit.isEmpty()) {
-                Span span = allSplit.pollFirst();
+            while (iterator.hasNext()) {
+                PennLikeSentenceTokenizer.TokenCandidate tokenCandidate = iterator.next();
+                Span span = tokenCandidate.toSpan();
                 if (span.length() == 0) {
                     continue;
                 }
